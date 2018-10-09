77e4d6875ce8ba4b4e3752118279804436091510
boolangery <eliott.dumeix@gmail.com>
2018-10-09 21:13:24 +0200

    update ddox config.

--- source/crypto/exchanges/package.d
+++ source/crypto/exchanges/package.d
@@ -1,3 +1,10 @@
+/**
+    Main package.
+    Import all exchanges.
+*/
+module crypto.exchanges;
+
+
 public import crypto.exchanges.core.api;
 public import crypto.exchanges.bittrex;
 public import crypto.exchanges.binance;
